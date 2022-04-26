ddc5cff39980b8d5d32fe4566807fa70a3cdde08
lucaslema21 <lucas.lema@cascade.app>
2022-04-26 16:39:51 -0300

    Multiple or singular states

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // let sarasa = 'Qué es React???';
-  const [sarasa, setSarasa] = useState('Qué es React???');
 
-  const updatear = () => {
-    // sarasa = 'Es una libreria de JS';
-    // console.log(sarasa)
-    setSarasa('Es una libreria de JS')
-    console.log(sarasa)
+  // const [email, setEmail] = useState('')
+  // const [name, setName] = useState('')
+  // const [password, setPassword] = useState('')
+
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    password: ''
+  })
+
+  const emailChangeHandler = (e) => {
+    setEmail(e.currentTarget.value)
   }
 
+  const nameChangeHandler = (e) => {
+    setName(e.currentTarget.value)
+  }
 
-
+  const passwordChangeHandler = (e) => {
+    setPassword(e.currentTarget.value)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {sarasa}
-        </a>
-        <button onClick={updatear}>Responder</button>
-      </header>
+      <form>
+        <div>
+          <label>Email</label>
+          <input type="email" onChange={emailChangeHandler} />
+          <p>Entered email is: {email}</p>
+        </div>
+        <div>
+          <label>Name</label>
+          <input type="text" onChange={nameChangeHandler} />
+          <p>Entered name is: {name}</p>
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" onChange={passwordChangeHandler} />
+          <p>Entered password is: {password}</p>
+        </div>
+      </form>
     </div>
   );
 }
